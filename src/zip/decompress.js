bca02bd72acdb620a039bb2fbe7749a5556ad69d
alexpodelinskii <nikitik00787@gmail.com>
2022-06-05 02:47:31 +0300

    feat: add solution to zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+
 export const decompress = async () => {
-    // Write your code here 
+    const unZip = createUnzip();
+    const archive = createReadStream('./files/archive.gz');
+    const textFile = createWriteStream('./files/fileToCompress.txt');
+
+    pipeline(
+        archive,
+        unZip,
+        textFile,
+        (err) => err ? console.error(err) : null
+    );
 };
+
+decompress();
