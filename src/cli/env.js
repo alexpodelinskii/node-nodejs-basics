c73249223abe10fc9c0c0f076d2b6a9dd8877132
alexpodelinskii <nikitik00787@gmail.com>
2022-10-16 01:19:46 +0300

    feat: start task

--- src/cli/env.js
+++
@@ -1,8 +0,0 @@
-export const parseEnv = () => {
-    for (let key in process.env) {
-        if (key.startsWith('RSS_')) {
-            console.log(`${key} = ${process.env[key]}`);
-        }
-    }
-};
-parseEnv()
